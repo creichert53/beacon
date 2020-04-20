c9e06dfbb1d90b16653a01c3e7d9c28f385874de
Chris Reichert <creichert@dynamix-ltd.com>
2020-04-20 11:54:14 -0400

    Remove an extra sudo in sudo-bootstrap.sh

--- dev-ops/sudo-bootstrap.sh
+++ dev-ops/sudo-bootstrap.sh
@@ -1,6 +1,6 @@
 # Update Linux Distro and install dependencies
 apt-get update
 apt-get -y upgrade
-apt-get -y sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+apt-get -y apt-get install -y make build-essential libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
   libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
