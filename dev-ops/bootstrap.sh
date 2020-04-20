8dfc14a346168c321af1d4cb76d78b6994872e15
Chris Reichert <creichert@dynamix-ltd.com>
2020-04-20 11:46:28 -0400

    Add pyenv install script.

--- dev-ops/bootstrap.sh
+++ dev-ops/bootstrap.sh
@@ -1,3 +1,11 @@
-apt-get update
-apt-get -y upgrade
-apt-get -y install git
+# Update Linux Distro and install dependencies
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+  libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+
+# Install pyenv
+curl https://pyenv.run | bash
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+exec $SHELL
--- dev-ops/install-project.sh
+++
@@ -1,1 +0,0 @@
-git clone https://github.com/creichert53/beacon.git
