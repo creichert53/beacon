36bd4fbe2b5ebe7c1f57765f2b36f77a942b94d8
Chris Reichert <creichert.53@gmail.com>
2020-04-21 00:19:37 -0400

    Added some packages and bootstrap lines.

--- dev-ops/sudo-bootstrap.sh
+++ dev-ops/sudo-bootstrap.sh
@@ -1,6 +1,9 @@
+#!/bin/bash 
+
 # Update Linux Distro and install dependencies
 apt-get update
 apt-get -y upgrade
-apt-get -y apt-get install -y make build-essential libssl-dev zlib1g-dev \
+apt-get -y install -y make build-essential libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-  libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+  libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl \
+  net-tools
--- dev-ops/user-bootstrap.sh
+++ dev-ops/user-bootstrap.sh
@@ -1,4 +1,20 @@
+#!/bin/bash 
+
 # Install pyenv
-curl https://pyenv.run | bash
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-exec $SHELL
+if [ -d "/home/$(whoami)/.pyenv" ]
+then
+  echo "It looks like .penv is already installed in the /home/{user}/.pyenv directory. If you'd like reinstall, simply delete this directory."
+else
+  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+  echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+  echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+  echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+  exec $SHELL
+fi
+
+pyenv install 3.7.5
+pyenv global 3.7.5
+pip install pipenv
+
+cd "/home/$(whoami)/beacon"
+pipenv install
