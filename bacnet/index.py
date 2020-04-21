7c9b6f23c7a04069ae089270e89e271d633a9f11
Chris Reichert <creichert@dynamix-ltd.com>
2020-04-21 17:55:55 -0400

    BAC0 just doesn't seem to work. Disconnect fails every single time with OSError: [Errno 9] Bad file descriptor. This might be a problem with running it on MacOS. Going to port to a Raspberry Pi to test.

--- bacnet/index.py
+++ bacnet/index.py
@@ -1,25 +1,77 @@
 import BAC0
-import tornado.ioloop
-import tornado.web
-import socket
+# import socket
+import time
+# import logging
 
-from tornado.options import define, options, parse_command_line
+# from tornado.escape import json_decode, json_encode
+# from tornado.ioloop import IOLoop
+# from tornado import gen
+# from tornado.options import define, options, parse_command_line
+# from tornado.web import Application, RequestHandler
 
-define("port", default=8888, help="run on the given port", type=int)
-define("debug", default=True, help="run in debug mode")
+# from tornado.options import define, options, parse_command_line
 
-# bacnet = BAC0.connect()
+# define("port", default=8888, help="run on the given port", type=int)
+# define("debug", default=True, help="run in debug mode")
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.connect(("8.8.8.8", 80))
+# ip_address = s.getsockname()[0]
+# s.close()
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+test = BAC0.connect(
+    ip="10.10.1.29/24"
+)
+test.disconnect()
 
-if __name__ == "__main__":
-    app = make_app()
-    app.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+# class BacnetDevice(object):
+#     def __init__(self):
+#         # self._daemon = BAC0.lite(
+#         #     ip=f"{ip_address}/24",
+#         #     port="47808",
+#         # )
+#         # self._daemon.disconnect()
+#         print("Hello")
+
+#     def reset(self):
+#         # fetch from database
+#         print("Trying to reset device")
+#         self._daemon.disconnect()
+#         # self._daemon = BAC0.lite(
+#         #     ip=f"{ip_address}/24",
+#         #     port="47808",
+#         # )
+
+#     def session(self):
+#         return self._daemon
+
+# class MainHandler(RequestHandler):
+#     def get(self):
+#         self.write("Hello, world")
+
+# class DiscoverHandler(RequestHandler):
+#     def initialize(self, bacnet_device):
+#         self._bacnet_device = bacnet_device
+
+#     def get(self):
+#         self._bacnet_device.reset()
+#         self.write("Something")
+
+# def main():
+#     parse_command_line(final=False)
+#     bacnet_device = BacnetDevice()
+
+#     app = Application(
+#         [
+#             (r"/", MainHandler),
+#             (r"/discover", DiscoverHandler, dict(bacnet_device=bacnet_device)),
+#         ],
+#         debug=options.debug
+#     )
+#     app.listen(options.port)
+
+#     print("Listening on http://localhost:%d" % options.port)
+#     IOLoop.current().start()
+
+# if __name__ == "__main__":
+#     main()
